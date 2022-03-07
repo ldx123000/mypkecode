2bddd6af82a456992dd90a18b4fb82061463fcac
ldx123000 <771484893@qq.com>
2022-03-07 16:19:02 +0800

    my work on lab3_3 is done.

--- kernel/strap.c
+++ kernel/strap.c
@@ -54,12 +54,7 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
   sprint("handle_page_fault: %lx\n", stval);
   switch (mcause) {
     case CAUSE_STORE_PAGE_FAULT:
-      // TODO (lab2_3): implement the operations that solve the page fault to
-      // dynamically increase application stack. 
-      // hint: first allocate a new physical page, and then, maps the new page to the
-      // virtual address that causes the page fault.
-      //panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
-      user_vm_map((pagetable_t)current->pagetable, stval, 1, (uint64)alloc_page(),
+      map_pages((pagetable_t)current->pagetable, stval, 1, (uint64)alloc_page(),
          prot_to_type(PROT_WRITE | PROT_READ, 1));
       break;
     default:
