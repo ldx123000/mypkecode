5d910d9b733ee22c0406c8b58b9870fa66558148
ldx123000 <771484893@qq.com>
2022-03-10 11:22:47 +0800

    my work on lab3_cha is co

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -68,6 +68,10 @@ ssize_t sys_user_fork() {
   return do_fork( current );
 }
 
+ssize_t sys_user_wait(uint64 pid) {
+  return wait( pid );
+}
+
 //
 // kerenl entry point of yield
 //
@@ -102,6 +106,8 @@ long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, l
       return sys_user_fork();
     case SYS_user_yield:
       return sys_user_yield();
+    case SYS_user_wait:
+      return sys_user_wait(a1);
     default:
       panic("Unknown syscall %ld \n", a0);
   }
