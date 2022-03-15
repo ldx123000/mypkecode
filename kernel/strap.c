e8ac86426109ce8c7caeb81410a63c3d759a06b8
ldx123000 <771484893@qq.com>
2022-03-15 06:12:41 +0000

    finish lab2_challenge1

--- kernel/strap.c
+++ kernel/strap.c
@@ -2,13 +2,14 @@
  * Utility functions for trap handling in Supervisor mode.
  */
 
-#include "riscv.h"
-#include "process.h"
 #include "strap.h"
-#include "syscall.h"
+#include "memlayout.h"
 #include "pmm.h"
-#include "vmm.h"
+#include "process.h"
+#include "riscv.h"
+#include "syscall.h"
 #include "util/functions.h"
+#include "vmm.h"
 
 #include "spike_interface/spike_utils.h"
 
@@ -25,9 +26,8 @@ static void handle_syscall(trapframe *tf) {
   // kernel/syscall.c) to conduct real operations of the kernel side for a syscall.
   // IMPORTANT: return value should be returned to user app, or else, you will encounter
   // problems in later experiments!
-  //panic( "call do_syscall to accomplish the syscall and lab1_1 here.\n" );
-  tf->regs.a0=do_syscall(tf->regs.a0,tf->regs.a1,tf->regs.a2,tf->regs.a3,tf->regs.a4,tf->regs.a5,tf->regs.a6,tf->regs.a7);
-
+  // panic( "call do_syscall to accomplish the syscall and lab1_1 here.\n" );
+  tf->regs.a0 = do_syscall(tf->regs.a0, tf->regs.a1, tf->regs.a2, tf->regs.a3, tf->regs.a4, tf->regs.a5, tf->regs.a6, tf->regs.a7);
 }
 
 //
@@ -38,9 +38,9 @@ void handle_mtimer_trap() {
   // TODO (lab1_3): increase g_ticks to record this "tick", and then clear the "SIP"
   // field in sip register.
   // hint: use write_csr to disable the SIP_SSIP bit in sip.
-  //panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip register.\n" );
+  // panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip register.\n" );
   g_ticks++;
-  write_csr(sip,0);
+  write_csr(sip, 0);
 }
 
 //
@@ -51,18 +51,24 @@ void handle_mtimer_trap() {
 void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
   sprint("handle_page_fault: %lx\n", stval);
   switch (mcause) {
-    case CAUSE_STORE_PAGE_FAULT:
-      // TODO (lab2_3): implement the operations that solve the page fault to
-      // dynamically increase application stack. 
-      // hint: first allocate a new physical page, and then, maps the new page to the
-      // virtual address that causes the page fault.
-      //panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+  case CAUSE_STORE_PAGE_FAULT:
+    // TODO (lab2_3): implement the operations that solve the page fault to
+    // dynamically increase application stack.
+    // hint: first allocate a new physical page, and then, maps the new page to the
+    // virtual address that causes the page fault.
+    // panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+    if (stval < USER_STACK_TOP - 20 * PGSIZE) {
+      sprint("this address is not available!\n");
+      shutdown(-1);
+    } else {
       map_pages((pagetable_t)current->pagetable, stval, 1, (uint64)alloc_page(),
-         prot_to_type(PROT_WRITE | PROT_READ, 1));
-      break;
-    default:
-      sprint("unknown page fault.\n");
-      break;
+                prot_to_type(PROT_WRITE | PROT_READ, 1));
+    }
+
+    break;
+  default:
+    sprint("unknown page fault.\n");
+    break;
   }
 }
 
@@ -73,7 +79,8 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
 void smode_trap_handler(void) {
   // make sure we are in User mode before entering the trap handling.
   // we will consider other previous case in lab1_3 (interrupt).
-  if ((read_csr(sstatus) & SSTATUS_SPP) != 0) panic("usertrap: not from user mode");
+  if ((read_csr(sstatus) & SSTATUS_SPP) != 0)
+    panic("usertrap: not from user mode");
 
   assert(current);
   // save user process counter.
@@ -83,23 +90,23 @@ void smode_trap_handler(void) {
   uint64 cause = read_csr(scause);
 
   switch (cause) {
-    case CAUSE_USER_ECALL:
-      handle_syscall(current->trapframe);
-      break;
-    case CAUSE_MTIMER_S_TRAP:
-      handle_mtimer_trap();
-      break;
-    case CAUSE_STORE_PAGE_FAULT:
-    case CAUSE_LOAD_PAGE_FAULT:
-      // the address of missing page is stored in stval
-      // call handle_user_page_fault to process page faults
-      handle_user_page_fault(cause, read_csr(sepc), read_csr(stval));
-      break;
-    default:
-      sprint("smode_trap_handler(): unexpected scause %p\n", read_csr(scause));
-      sprint("            sepc=%p stval=%p\n", read_csr(sepc), read_csr(stval));
-      panic( "unexpected exception happened.\n" );
-      break;
+  case CAUSE_USER_ECALL:
+    handle_syscall(current->trapframe);
+    break;
+  case CAUSE_MTIMER_S_TRAP:
+    handle_mtimer_trap();
+    break;
+  case CAUSE_STORE_PAGE_FAULT:
+  case CAUSE_LOAD_PAGE_FAULT:
+    // the address of missing page is stored in stval
+    // call handle_user_page_fault to process page faults
+    handle_user_page_fault(cause, read_csr(sepc), read_csr(stval));
+    break;
+  default:
+    sprint("smode_trap_handler(): unexpected scause %p\n", read_csr(scause));
+    sprint("            sepc=%p stval=%p\n", read_csr(sepc), read_csr(stval));
+    panic("unexpected exception happened.\n");
+    break;
   }
 
   // continue the execution of current process.
