9dba4730a0e0854df57f8722deef09c98eae6556
ldx123000 <771484893@qq.com>
2022-06-04 13:58:05 +0000

    finish lab3_challenge4

--- user/app_cyclicbarrier.c
+++ user/app_cyclicbarrier.c
@@ -1,6 +1,6 @@
 /*
  * This app create two child process.
- * Use semaphores to control the order of
+ * Use cyclicbarrier to control the order of
  * the main process and two child processes print info.
  */
 #include "user/user_lib.h"
@@ -8,50 +8,21 @@
 
 int main(void) {
   int pid = fork();
-  if (pid == 0) { //子
+  if (pid == 0) {
     pid = fork();
     if (pid == 0) {
       printu("Process2 print %d\n", 0);
       cyclicbarrier(3);
       printu("Process2 print %d\n", 1);
-      // cyclicbarrier(2);
-      // printu("Process3 print %d\n", 2);
     } else {
       printu("Process1 print %d\n", 0);
       cyclicbarrier(3);
       printu("Process1 print %d\n", 1);
-      // // cyclicbarrier(3);
-      // printu("Process1 print %d\n", 2);
     }
-    // for (int i = 0; i < 10; i++) {
-    //   sem_P(child_sem[pid == 0]); // child_sem[0 or 1]
-    //   printu("Child%d print %d\n", pid == 0, i);
-    //   if (pid != 0)//child 1
-    //     sem_V(child_sem[1]);
-    //   else//child 0
-    //     sem_V(main_sem);
-    // }
-  } else { //父
-    
-    // if (pid == 0) {
-    //   printu("Process2 print %d\n", 0);
-
-    //   printu("Process2 print %d\n", 1);
-
-    //   printu("Process2 print %d\n", 2);
-    // } else {
-      printu("Process0 print %d\n", 0);
-      cyclicbarrier(3);
-      printu("Process0 print %d\n", 1);
-      // cyclicbarrier(2);
-      // printu("Process0 print %d\n", 2);
-    // }
-
-    // for (int i = 0; i < 10; i++) {
-    //   sem_P(main_sem);
-    //   printu("Parent print %d\n", i);
-    //   sem_V(child_sem[0]);
-    // }
+  } else {
+    printu("Process0 print %d\n", 0);
+    cyclicbarrier(3);
+    printu("Process0 print %d\n", 1);
   }
   exit(0);
   return 0;
